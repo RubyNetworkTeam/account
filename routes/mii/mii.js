0fa2a6777f0629510c4504a4d77e292edf6874f5
landonatminerosoft <landonatminerosoft@gmail.com>
2023-12-07 13:49:54 -0500

    Add compatibility to older nodejs releases

--- routes/mii/mii.js
+++ routes/mii/mii.js
@@ -1,3 +1,18 @@
+var fetch
+var addfetch = 0
+
+try {
+    fetch()
+} catch (e) {
+    if (e instanceof ReferenceError) {
+        addfetch=1
+    }
+}
+
+if(addfetch != 1 ) {
+    fetch = require('node-fetch')
+}
+
 const express = require('express')
 const router = express.Router()
 const util = require('util')
@@ -18,20 +33,18 @@ router.get('/:rpid/:file', async (req, res) => {
     const data = await query(`SELECT mii_hash1 FROM accounts WHERE pid="${rpid}"`)
     if (data.length == 0) {
     res.status = 404
-    // This could be optimizied, but Rubies doesnt give me permission :D
     return res.send(`<html><body><style type="text/css">p {word-wrap: break-word;}</style><center><h1 style="font-family: tahoma;">|___=+404+=___|</h1></center><p id="d"></p><script>window.setInterval("document.getElementById('d').innerHTML +=' &#'+Math.floor((Math.random() * 10000) + 1)+';';", 100);</script></body></html>`)
     }
     const mii_data = data[0].mii_hash1
     const mii = new Mii(Buffer.from(mii_data, 'base64'))
     let studioUrl = mii.studioUrl()
     const request = await fetch(studioUrl);
-    const mata = await request.arrayBuffer()
-    const out = new Uint8Array(mata)
+    const out = await request.arrayBuffer()
+    const buff = Buffer.from(out)
    res.writeHead(200, {
-     'Content-Type': 'image/png',
-     'Content-Length': out.length
+     'Content-Type': 'image/png'
    });
-   res.end(out)
+   res.end(buff)
 })
 
 module.exports = router;
