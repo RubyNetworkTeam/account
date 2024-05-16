c0ebbdc4ea8e79d8320063b6d1777c09744ece5b
#Wifi Got Minecraft <64691261+PokeyManatee4@users.noreply.github.com>
2024-05-16 19:33:22 +0000

    Update router.js

--- routes/api/v1/router.js
+++ routes/api/v1/router.js
@@ -24,21 +24,21 @@ v1_Router.use((req, res, next) => {
     next();
 })
 
-v1_Router.use('/content/agreements/Nintendo-Network-EULA/:var/@latest', eulaRoute)
-v1_Router.use('/devices/@current/status', statusRoute)
-v1_Router.use('/content/time_zones/:var/:var', timezoneRoute)
-//v1_Router.use('/people', rnidRoute)
-//v1_Router.use('/people/@me/devices/', rnidRoute)
-v1_Router.use('/support/validate/email', emailRoute)
-v1_Router.use('/admin/time', emailRoute)
-v1_Router.use('/people/', peopleRoute)
-v1_Router.use('/oauth20/access_token/generate', serviceRoute)
-v1_Router.use('/people/@me/profile', infoRoute)
-v1_Router.use('/people/@me', infoRoute)
-v1_Router.use('/provider/nex_token/@me', gameRoute)
-v1_Router.use('/admin/mapped_ids', friendRoute)
-v1_Router.use('/', miiRoute)
-v1_Router.use('/provider/service_token/@me', tokenRoute)
+v1_Router.use('/content/agreements/Nintendo-Network-EULA/:var/@latest', eulaRoute);
+v1_Router.use('/devices/@current/status', statusRoute);
+v1_Router.use('/content/time_zones/:var/:var', timezoneRoute);
+v1_Router.use('/people', rnidRoute);
+v1_Router.use('/people/@me/devices/', rnidRoute);
+v1_Router.use('/support/validate/email', emailRoute);
+v1_Router.use('/admin/time', emailRoute);
+v1_Router.use('/people/', peopleRoute);
+v1_Router.use('/oauth20/access_token/generate', serviceRoute);
+v1_Router.use('/people/@me/profile', infoRoute);
+v1_Router.use('/people/@me', infoRoute);
+v1_Router.use('/provider/nex_token/@me', gameRoute);
+v1_Router.use('/admin/mapped_ids', friendRoute);
+v1_Router.use('/', miiRoute);
+v1_Router.use('/provider/service_token/@me', tokenRoute);
 v1_Router.use('/people/@me/devices/owner', owner_router);
 
 module.exports = v1_Router;
